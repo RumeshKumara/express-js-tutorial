a20337cb454c628ed6cc0859051808057e61c634
rumesh <rumeshk066@gmail.com>
2025-07-30 16:00:33 +0530

    fist commit

--- server.mjs
+++ server.mjs
@@ -4,7 +4,11 @@ import express from 'express';
 const server = express();
 
 server.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.json({ message: 'Hello, World!' });
+});
+
+server.get('/api/v1/about', async (req, res) => {
+    res.json({ message: 'This is the about page.' });
 });
 server.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
